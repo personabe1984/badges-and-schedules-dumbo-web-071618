952fd0c0e68695e0b8920ab50e15fbd9f1c907bf
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-02 18:59:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badges(attendees).each do |badge|
+  batch_badge_creator(attendees).each do |badge|
     puts badge_maker
   end
   
